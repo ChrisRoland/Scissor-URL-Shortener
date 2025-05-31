79fc49533336bf6b76b08fed23c7c6d3ef9c00c5
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-31 18:40:34 +0100

    feat: revamped navbar
    Updated navbar css to fix styling issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,27 @@
+import { ChevronDown } from 'lucide-react';
 import logo from '../assets/media/SciLogo.png';
 
 export default function NavBar() {
   return (
-    <header className="bg-white shadow">
-      <nav className="flex items-center justify-between px-8 py-4">
-        <div className="flex items-center gap-2 text-2xl font-bold text-blue-600">
-          <img src={logo} alt="Logo" className="h-6 w-6" />|
-          SCISSOR
+    <header className="">
+      <nav className="flex justify-between items-center px-[90px] py-[20px]">
+        <div className="flex justify-center items-center gap-[5px] text-[24px] font-semibold text-[#0065FE]">
+          <img src={logo} alt="Logo" className="h-6 w-6" /> <span className='font-normal'> | </span> SCISSOR
         </div>
-        <ul className="hidden md:flex gap-8 text-gray-800">
-          <li><a href="#" className="text-blue-600">My URLs</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#pricing">Pricing</a></li>
-          <li><a href="#analytics">Analytics</a></li>
-          <li><a href="#faqs">FAQs</a></li>
+        <ul className="flex list-none gap-[40px]">
+          <li><a href="#" className="active decoration-0 text-[#0065fe]">My URLs</a></li>
+          <li className="">
+            <a href="#" className="decoration-0 text-black flex justify-center items-center gap-[2px]">Features <ChevronDown size={18}/></a>
+          </li>
+          <li><a href="#" className="decoration-0 text-black">Pricing</a></li>
+          <li><a href="#" className="decoration-0 text-black">Analytics</a></li>
+          <li><a href="#" className="decoration-0 text-black">FAQs</a></li>
         </ul>
-        <div className="hidden md:flex gap-4">
-          <a href="#" className="text-blue-600">Log in</a>
-          <button className="rounded-full bg-blue-600 px-6 py-2 text-white hover:bg-blue-700">
-            Try for free
-          </button>
+        <div className="flex justify-center items-center gap-[20px]">
+          <a href="#" className="decoration-0 text-[#007bff]">Log in</a>
+          <button className="btn btn-primary">Try for free</button>
         </div>
       </nav>
     </header>
-    );
+  );
 }
